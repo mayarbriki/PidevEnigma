9aefef2c6a2965cd26b7b3696c8e93c541fe7d9e
mayarbriki <100524582+mayarbriki@users.noreply.github.com>
2024-02-25 16:06:30 +0100

    integration user+produit

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -63,6 +63,7 @@ class ProduitController extends AbstractController
 
 
   
+
    
     #[Route('/produit/add', name: 'add1')]
     public function add1(ManagerRegistry $doctrine,Request $request, SluggerInterface $slugger): Response
